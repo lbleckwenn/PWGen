600cb50078867b05cd131b49712057df9639e7c1
Lars Bleckwenn <lars.bleckwenn@web.de>
2019-12-20 11:12:45 +0100

    Cookie disclaimer

--- templates/main.php
+++ templates/main.php
@@ -5,7 +5,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.css" />
 <title><?php echo $this->_['main_title']; ?></title>
 <style>
 main>.container {
@@ -50,5 +50,27 @@ main>.container {
 
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.js" data-cfasync="false"></script>
+	<script>
+		window.cookieconsent.initialise({
+			"palette": {
+				"popup": {
+					"background": "#237afc"
+				},
+				"button": {
+					"background": "#fff",
+					"text": "#237afc"
+				}
+			},
+			"theme": "classic",
+			"position": "bottom-right",
+			"content": {
+				"message": "Diese Webseite benutzt Cookies um benutzerdefinierte Einstellungen zur generierten Passwörtern zu speichern.",
+				"dismiss": "Verstanden",
+				"link": "Weitere Informationen",
+				"href": "?page=privacy"
+			}
+		});
+	</script>
 </body>
 </html>
